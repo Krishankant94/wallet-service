1a5a8d1296a7bda47c3536fa4b43a8e0d02e51c8
kksharma <kkparashar78@gmail.com>
2023-07-31 14:15:46 +0530

    Update asyncWrapper.js

--- middleware/asyncWrapper.js
+++ middleware/asyncWrapper.js
@@ -1,5 +1,14 @@
 const asyncWrapper = (fn) => {
     return async (req, res, next) => {
+        res.setHeader('Access-Control-Allow-Credentials', true)
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  )
       try {
         await fn(req, res, next)
       } catch (error) {
